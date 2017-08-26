55f517201f39d221e0874da1fc8ea140b0cf12ec
Salwe Breid <salwebreid@gmail.com>
2017-08-26 20:23:56 +0400

    Project initializing

---
+++ README.md
@@ -0,0 +1,17 @@
+## Empty React App
+
+This is the first project for learning react
+
+Usage:
+
+```
+git clone https://github.com/salwe/reactApp
+```
+
+```
+npm install
+```
+
+```
+npm start
+```
---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import { render } from "react-dom";
+
+class App extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Hi there</h1>
+            </div>
+        )
+    }
+}
+render(<App/>, document.getElementById("content"));
